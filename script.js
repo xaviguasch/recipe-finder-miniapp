d05c49999ccdc214512e57876c37b8c1a8af9188
xaviguasch <xaviguasch@gmail.com>
2020-02-18 09:40:25 +0100

    made API requests to the meal endpoint

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('meal finder!!!!')
+const search = document.getElementById('search')
+const submit = document.getElementById('submit')
+const random = document.getElementById('random')
+const mealsEl = document.getElementById('meals')
+const resultHeding = document.getElementById('result-heading')
+const single_mealEl = document.getElementById('single-meal')
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault()
+
+  // Clear single meal
+  single_mealEl.innerHTML = ''
+
+  // Get search term
+  const term = search.value
+
+  // Check for empty
+
+  if (term.trim()) {
+    const meal = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+
+    console.log(meal)
+  } else {
+    alert('Please enter a search value')
+  }
+}
+
+// Event listeners
+submit.addEventListener('submit', searchMeal)
